af933fa8d5ec49e9a983ae40845f5dffccb36dee
Pavel Lezin <pavellezin@yandex.ru>
2020-05-26 13:16:10 +0500

    HW4 v1.1 add_jpa_meal_repo

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -8,9 +8,32 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m"
+                + " WHERE m.id=:id"
+                + " AND m.user.id=:userId"),
+        @NamedQuery(name = Meal.GET, query = "SELECT m FROM Meal m"
+                + " WHERE m.id=:id"
+                + " AND m.user.id=:userId"),
+        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m"
+                + " WHERE m.user.id=:userId"
+                + " ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.BETWEEN, query = "SELECT m FROM Meal m"
+                + " WHERE m.user.id=:userId"
+                + " AND m.dateTime>=:startDate"
+                + " AND m.dateTime<:endDate"
+                + " ORDER BY m.dateTime DESC")
+})
+
 @Entity
+//@Table(name = "meals")
 @Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time"}, name = "meals_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
+
+    public static final String DELETE = "Meal.delete";
+    public static final String GET = "Meal.get";
+    public static final String ALL_SORTED = "Meal.getAllSorted";
+    public static final String BETWEEN = "Meal.getBetween";
 
     @Column(name = "date_time", nullable = false, unique = true, columnDefinition = "timestamp")
     private LocalDateTime dateTime;
