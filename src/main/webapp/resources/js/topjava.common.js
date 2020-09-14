d2769ae293d1697347663f6dcffc4e7b4df4687a
Pavel Lezin <pavellezin@yandex.ru>
2020-09-14 11:39:26 +0500

    HW9+Optional add_custom_converter

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -8,13 +8,7 @@ function makeEditable(ctx) {
     });
 
     // solve problem with cache in IE: https://stackoverflow.com/a/4303862/548473
-    $.ajaxSetup({cache: false,
-        converters: {
-            "text json": function (data) {
-                return $.parseMyJSON(data, true);
-            }
-        }
-    });
+    $.ajaxSetup({cache: false});
 }
 
 function add() {
@@ -50,15 +44,11 @@ function updateTableByData(data) {
 }
 
 function save() {
+    var myJSONData = form.serialize();
     $.ajax({
         type: "POST",
         url: context.ajaxUrl,
-        data: form.serialize(),
-        converters: {
-            "text json": function (data) {
-                return $.parseMyJSON(data, true);
-            }
-        }
+        data: toISOConverter(myJSONData)
     }).done(function () {
         $("#editRow").modal("hide");
         context.updateTable();
@@ -106,91 +96,11 @@ function renderDeleteBtn(data, type, row) {
     }
 }
 
-/*!
- * jQuery.parseJSON() extension (supports ISO & Asp.net date conversion)
- *
- * Version 1.0 (13 Jan 2011)
- *
- * Copyright (c) 2011 Robert Koritnik
- * Licensed under the terms of the MIT license
- * http://www.opensource.org/licenses/mit-license.php
- */
-(function ($) {
-
-    // JSON RegExp
-    var rvalidchars = /^[\],:{}\s]*$/;
-    var rvalidescape = /\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g;
-    var rvalidtokens = /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g;
-    var rvalidbraces = /(?:^|:|,)(?:\s*\[)+/g;
-    // var dateISO = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:[.,]\d+)?Z?/i;
-    var dateISO = /\d{4}-[01]\d-[0-3]\d [0-2]\d:[0-5]\d/;
-    var dateNet = /\/Date\((\d+)(?:-\d+)?\)\//i;
-
-    // replacer RegExp
-    var replaceISO = /"(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:[.,](\d+))?Z?"/i;
-    var replaceNet = /"\\\/Date\((\d+)(?:-\d+)?\)\\\/"/i;
-
-    // determine JSON native support
-    var nativeJSON = (window.JSON && window.JSON.parse) ? true : false;
-    var extendedJSON = nativeJSON && window.JSON.parse('{"x":9}', function(k,v){return "Y";}) === "Y";
-
-    var jsonDateConverter = function(key, value) {
-        if (key === "dateTime") {
-        // if (typeof(value) === "string") {
-            return "2020-09-14T00:16";
-/*            if (dateISO.test(value)) {
-                if (value == '0001-01-01T00:00:00') {
-                    return null;
-                }
-
-                // return new Date(value);
-                return value.substring(0, 10) + "T" + value.substring(11, 16);
-            }
-            if (dateNet.test(value))
-            {
-                return new Date(parseInt(dateNet.exec(value)[1], 10));
-            }*/
-        }
-        return value;
-    };
-
-    $.extend({
-        parseMyJSON: function(data, convertDates) {
-            /// <summary>Takes a well-formed JSON string and returns the resulting JavaScript object.</summary>
-            /// <param name="data" type="String">The JSON string to parse.</param>
-            /// <param name="convertDates" optional="true" type="Boolean">Set to true when you want ISO/Asp.net dates to be auto-converted to dates.</param>
-
-            if (typeof data !== "string" || !data) {
-                return null;
-            }
-
-            // Make sure leading/trailing whitespace is removed (IE can't handle it)
-            data = $.trim(data);
-
-            // Make sure the incoming data is actual JSON
-            // Logic borrowed from http://json.org/json2.js
-            if (rvalidchars.test(data
-                .replace(rvalidescape, "@")
-                .replace(rvalidtokens, "]")
-                .replace(rvalidbraces, "")))
-            {
-                // Try to use the native JSON parser
-
-                if (extendedJSON || (nativeJSON && convertDates !== true))
-                {
-                    return window.JSON.parse(data, convertDates === true ? jsonDateConverter : undefined);
-                }
-                else {
-                    data = convertDates === true ?
-                        data.replace(replaceISO, "new Date(parseInt('$1',10),parseInt('$2',10)-1,parseInt('$3',10),parseInt('$4',10),parseInt('$5',10),parseInt('$6',10),(function(s){return parseInt(s,10)||0;})('$7'))")
-                            .replace(replaceNet, "new Date($1)"):
-                        data;
-                    return (new Function("return " + data))();
-                }
-            } else
-            {
-                $.error("Invalid JSON: " + data);
-            }
-        }
-    });
-})(jQuery);
+function toISOConverter(data) {
+    var ISODate;
+    let regexp = /(\d{4})-(\d{2})-(\d{2})%20(\d{2})%3A(\d{2})/;
+    let formDate = data.match(regexp)[0];
+    ISODate = formDate.substring(0, 10) + "T" + formDate.substring(13, 15) + ":" + formDate.substring(18, 20);
+    // debugger;
+    return data.replace(regexp, ISODate);
+}
